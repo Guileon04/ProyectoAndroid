bd7864b02ff7fb0a57a6f8cbfffd63a1bb47a0be
Guillermo <guilleleonmoral@gmail.com>
2025-11-06 18:24:22 +0100

    T1GLM

---
+++ app/src/main/java/com/example/tarea1glm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tarea1glm
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
